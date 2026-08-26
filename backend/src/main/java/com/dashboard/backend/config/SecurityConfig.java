06eab73eca29ddef382dabe80ca68fd9217c5312
Anwar Imam <anwar.imam412@gmail.com>
2026-08-26 12:16:57 +0530

    Removed production origin for ec2 deployment

--- backend/src/main/java/com/dashboard/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/dashboard/backend/config/SecurityConfig.java
@@ -78,8 +78,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
         	    "http://localhost:3000",
-        	    "http://localhost:5173",
-                "http://15.207.55.33",
+        	    "http://localhost:5173"
         	));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
